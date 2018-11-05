a6d3c2ee54bfe17a0e57ee99824329241241ee7d
mhm <289562326@qq.com>
2018-11-05 10:41:28 +0800

    springbootdemo3

---
+++ src/main/java/com/mhm/Application.java
@@ -0,0 +1,16 @@
+package com.mhm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 定时任务的工程
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/mhm/task/HeartBeatTask.java
@@ -0,0 +1,7 @@
+package com.mhm.task;
+
+/**
+ * Created by MaHuiming on 2018/11/4.
+ */
+public class HeartBeatTask {
+}
